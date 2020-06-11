e94c053b7ba61a79a30a8f540bfcc2faab9494bf
samuel.swanson <samuel.swanson@valid.com>
2020-06-11 15:03:35 -0400

    environment map public repo for org consumption

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+var ref = github.context.ref;
+ref = ref.replace("refs/heads/","");
+
+console.log(ref);
+switch (ref) {
+  case /master/.test(ref) && ref:
+    var env = "DEV";
+    var altEnv = "develop";
+    break;
+  case /^(release-*(\d*))/.test(ref) && ref:
+    var env = "QA";
+    var altEnv = "qa";
+    break;
+  default:
+    var env = "BRANCH";
+    var altEnv = "none";
+}
+core.setOutput("environment", env);
+core.setOutput("altEnvironment", altEnv);
